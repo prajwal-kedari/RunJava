d6c1d8b3265616411460b16c7e94080df49b653f
prajwal-kedari <96010758+prajwal-kedari@users.noreply.github.com>
2022-02-06 19:29:42 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,14 @@
+import java.time.LocalDateTime;
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        LocalDateTime now = LocalDateTime.now();
+        String min   = String.valueOf( now.getMinute()     *2  );
+        String month = String.valueOf( now.getMonthValue() *60 );
+        String year  = String.valueOf( now.getYear()       *7  );
+        String hour  = String.valueOf( now.getHour()       *4  );
+        String day   = String.valueOf( now.getDayOfMonth() *9  );
+        String  strkey   = min+month+year+hour+day;
+        System.out.println("key:- "+ strkey);
     }
 }
