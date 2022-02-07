228d4d613fd51c055f0ce6701c1cc52c1b12a413
prajwal-kedari <96010758+prajwal-kedari@users.noreply.github.com>
2022-02-07 11:14:38 +0530

    Correct #1

--- Generator.java
+++ Generator.java
@@ -1,7 +1,7 @@
 import java.time.LocalDateTime;
+import java.math.BigInteger;
 public class  Generator{
 public static void  main(String arg[]){
-System.out.println("Hello World");
 LocalDateTime now = LocalDateTime.now();
 String min   = String.valueOf( now.getMinute()     *2  );
 String month = String.valueOf( now.getMonthValue() *60 );
@@ -9,6 +9,7 @@ String year  = String.valueOf( now.getYear()       *7  );
 String hour  = String.valueOf( now.getHour()       *4  );
 String day   = String.valueOf( now.getDayOfMonth() *9  );
 String  strkey   = min+month+year+hour+day;
-System.out.println(strkey);
+BigInteger Key   = new BigInteger (strkey);
+System.out.println("key:- "+Key);
 }
 }
