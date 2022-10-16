389a98e92461175100ca77747f04600412999557
Prajwal Kedari <65669641+prajwalkedari@users.noreply.github.com>
2022-10-17 01:25:05 +0530

    Key Correct Error , #1
    this Not an Error jsut addd 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,12 +3,12 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         LocalDateTime now = LocalDateTime.now();
-        String min   = String.valueOf( now.getMinute()     *2  );
-        String month = String.valueOf( now.getMonthValue() *60 );
-        String year  = String.valueOf( now.getYear()       *7  );
-        String hour  = String.valueOf( now.getHour()       *4  );
-        String day   = String.valueOf( now.getDayOfMonth() *9  );
-        String  strkey   = min+month+year+hour+day;
-        System.out.println("key:- "+ strkey);
+        String min1   = String.valueOf( now.getMinute()     *2  );
+        String month1 = String.valueOf( now.getMonthValue() *60 );
+        String year1  = String.valueOf( now.getYear()       *7  );
+        String hour1  = String.valueOf( now.getHour()       *4  );
+        String day1   = String.valueOf( now.getDayOfMonth() *9  );
+        String  strkey1   = min1+month1+year1+hour1+day1;
+        System.out.println("key will this= > "+ strkey);
     }
 }
